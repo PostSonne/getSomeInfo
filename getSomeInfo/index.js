ee6bc9116005bc5ce7edf3a9c84b18595fcb67a4
PostSonne <abiramova@gmail.com>
2020-01-31 00:02:23 +0300

    first script

--- getSomeInfo/index.js
+++
@@ -1,7 +0,0 @@
-const osmosis =  require("osmosis");
-
-osmosis
-    .get("https://www.last.fm/ru/music")
-    .find("#mantle_skin")
-    .set({"related": [".music-more-tags-tag-link"]})
-    .data((data) => {console.log(data)});
