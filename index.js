6c4c4ea86d07e07029d8cb5e7f7e8e364ab5a1c5
PostSonne <abiramova@gmail.com>
2020-02-01 01:32:42 +0300

    parsed info

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const osmosis =  require("osmosis");
-
+const osmosis = require("osmosis");
+const fs = require("fs");
+let savedData = [];
 osmosis
-    .get("https://www.last.fm/ru/music")
-    .find("#mantle_skin")
-    .set({"related": [".music-more-tags-tag-link"]})
-    .data((data) => {console.log(data)});
+    .get("https://www.last.fm/ru/user/Madam_Kaplan/loved")
+    .paginate(".pagination ul > li a[href]", 12)
+    .find("#user-loved-tracks-section")
+    .set({"Loved Tracks": ["table tbody tr .chartlist-name a"]})
+    .log(console.log)
+    .data((data) => {
+        console.log(data);
+        savedData.push(data);
+    })
+    .done(() => {
+        fs.writeFile("data.json", JSON.stringify)(savedData, null, 4),
+        (err) => {
+            err ? console.error(err) : console.log("Succesfully saved")
+        }
+    })
